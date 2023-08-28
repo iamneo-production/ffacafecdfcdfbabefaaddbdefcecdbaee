5f2cd66a10ead712b4cc8b5f68fbd260984aac6f
Iamneo <techadmin@iamneo.ai>
2023-08-28 07:36:12 +0000

    Backup Commit

---
+++ public/index.js
@@ -0,0 +1,22 @@
+document.getElementById("depositBtn").addEventListener("click", function(event) {
+    event.preventDefault();
+    
+    var accountNumber = document.getElementById("accountNumber").value;
+    var amount = document.getElementById("amount").value;
+    
+    // Perform deposit operation based on the user input
+        // This involves using the accountNumber and amount variables, which store the user's input from the account number and amount fields. 
+        // Update the display area with a message indicating a successful deposit along with the account number and amount.
+   
+  });
+  
+  document.getElementById("withdrawBtn").addEventListener("click", function(event) {
+    event.preventDefault();
+    
+    // Perform withdraw operation based on the user input
+        // Inside this section, write the code to handle the withdrawal operation. 
+        // You'll use the same accountNumber and amount variables, as well as the input fields. 
+        // Update the display area to show a message confirming a successful withdrawal, along with the account number and amount.
+    
+  });
+  
